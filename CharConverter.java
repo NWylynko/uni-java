c172decae157ae82e082f2021c38fc494bb6af0f
Nick Wylynko <nick@wylynko.com>
2023-03-09 11:32:32 +0000

    Functions yayyyy

--- CharConverter.java
+++ CharConverter.java
@@ -4,24 +4,46 @@ import java.util.regex.Pattern;
 
 public class CharConverter {
   public static void main(String[] args) {
+    var input = getInput();
+
+    try {
+      validateInput(input);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return;
+    }
+
+    int asciiValue = getAsciiValue(input);
+    char lowerChar = getLowerChar(input);
+
+    System.out.println("The ASCII value of '" + input + "' is: " + asciiValue);
+    System.out.println("The lowercase value of '" + input + "' is: '" + lowerChar + "'");
+  }
+
+  public static String getInput() {
     System.out.print("Please enter an uppercase character: ");
     Scanner scan = new Scanner(System.in);
     String input = scan.nextLine();
     scan.close();
+    return input;
+  }
 
+  public static void validateInput(String input) throws Exception {
     Pattern allowedChars = Pattern.compile("[A-Z]");
     Matcher match = allowedChars.matcher(input);
 
     if (!match.matches()) {
-      System.out.println("You need to enter a single, uppercase character");
-      return;
+      throw new Exception("You need to enter a single, uppercase character");
     }
+  }
 
+  public static int getAsciiValue(String input) {
     char character = input.charAt(0);
-    int asciiValue = (int) character;
-    char lowerChar = (char) (asciiValue + 32);
+    return (int) character;
+  }
 
-    System.out.println("The ASCII value of '" + input + "' is: " + asciiValue);
-    System.out.println("The lowercase value of '" + input + "' is: '" + lowerChar + "'");
+  public static char getLowerChar(String input) {
+    int asciiValue = getAsciiValue(input);
+    return (char) (asciiValue + 32);
   }
 }
